1e3c2c49c595729d14c98830dcad9d9d583c6af3
Aashutosh Thapa <aashutoshthapa@Aashutoshs-MacBook-Air.local>
2025-05-28 17:51:44 +0545

    Revert "feat: implement new landing page and refine UI based on design - Add new LandingPage component - Update routing to display LandingPage at root - Apply new color scheme and UI/UX to components - Improve mobile responsiveness of various sections"
    This reverts commit 350ab3cc3c42b324f004d748f984f38ba06b2293.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -8,7 +9,6 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import Index from "./pages/Index";
 import AdminPanel from "./pages/AdminPanel";
 import NotFound from "./pages/NotFound";
-import LandingPage from "./pages/LandingPage";
 
 const queryClient = new QueryClient();
 
@@ -21,7 +21,7 @@ const App = () => (
           <Sonner />
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<LandingPage />} />
+              <Route path="/" element={<Index />} />
               <Route path="/admin" element={<AdminPanel />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
